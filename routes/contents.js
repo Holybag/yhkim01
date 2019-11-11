22bed585b4a8b3acb9d9547811b064afa58a1874
yhkim <hohokim@Yonghoui-MacBookPro.local>
2019-11-11 17:16:40 +0100

    contents finish

--- routes/contents.js
+++ routes/contents.js
@@ -16,7 +16,7 @@ var pool  = mysql.createPool({
 /* GET users listing. */
 router.get('/', function(req, res, next) {
 
-	pool.query('SELECT * FROM Users', function(error, results, fields){
+	pool.query('SELECT * FROM Contents', function(error, results, fields){
 		if (error) res.send(error);
 		res.send(results);
 	});
@@ -27,7 +27,7 @@ router.get('/:id', function(req, res, next){
 	
 	var id = req.params.id;
 	pool.query({
-		sql: 'SELECT * FROM Users WHERE id=?',
+		sql: 'SELECT * FROM Contents WHERE id=?',
 		timeout: 4000,
 		values: [id]
 	}, function(error, results, fields){
@@ -39,51 +39,54 @@ router.get('/:id', function(req, res, next){
 	});
 });
 
+router.post('/', function(req, res, next){
+	
+	var title = req.body.title;
+	var type = req.body.type;
+	var contents = req.body.contents;
+	var image_url = req.body.image_url;
+	var user_id = req.body.user_id;
+	pool.query({
+		sql: 'INSERT INTO Contents (title, type, contents, image_url, user_id, created_at)\
+		                  Values (?, ?, ?, ?, ?, NOW())',
+		timeout: 4000,
+		values: [title, type, contents, image_url, user_id]
+	}, function(error, results, fields){
+		if (error) {
+			res.send(error);
+		} else {
+			res.send('Contents ' + title + ' is added');
+		}
+	});
+})
+
 router.delete('/', function(req, res, next){
 	pool.query({
-		sql: 'DELETE FROM Users',
+		sql: 'DELETE FROM Contents',
 		timeout: 4000
 	}, function(error, results, fields){
 		if (error) {
 			res.send(error);
 		} else {
-			res.send('All User is deleted');
+			res.send('All Contents are deleted');
 		}
 	});
 });
 
 router.delete('/:id', function(req, res, next){
 	var id = req.params.id;
+	console.log(id);
 	pool.query({
-		sql: 'DELETE FROM Users WHERE id=?',
+		sql: 'DELETE FROM Contents WHERE id=?',
 		timeout: 4000,
 		values: [id]
 	}, function(error, results, fields){
 		if (error) {
 			res.send(error);
 		} else {
-			res.send('User ' + id + ' is deleted');
+			res.send('Content ' + id + ' is deleted');
 		}
 	});
 });
-
-router.post('/', function(req, res, next){
-	
-	var email = req.body.email;
-	var password = req.body.password;
-	var name = req.body.name;
-	pool.query({
-		sql: 'INSERT INTO Users (email, password, name, created_at)\
-		                  Values (?, ?, ?, NOW())',
-		timeout: 4000,
-		values: [email, password, name]
-	}, function(error, results, fields){
-		if (error) {
-			res.send(error);
-		} else {
-			res.send('User ' + name + ' is registed');
-		}
-	});
-})
 
 module.exports = router;
