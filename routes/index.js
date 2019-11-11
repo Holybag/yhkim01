8de829cc90f48a03c2e9b5d68369af3d2c805f1f
yhkim <hohokim@Yonghoui-MacBookPro.local>
2019-11-11 17:34:20 +0100

    login finish

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,18 @@
 var express = require('express');
-var bodyParser = require('body-parser')
+var bodyParser = require('body-parser');
+var mysql = require('mysql');
 
 var router = express.Router();
 
 router.use(bodyParser.urlencoded({ extended: false }));
+
+var pool  = mysql.createPool({
+  connectionLimit : 10,
+  host            : 'localhost',
+  user            : 'yhkim',
+  password        : 'yhkim01!',
+  database        : 'yhkim01'
+});
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
@@ -11,9 +20,29 @@ router.get('/', function(req, res, next) {
 });
 
 router.post('/login', function(req, res, next) {
-	console.log(req.body.id);
+	console.log(req.body.email);
 	console.log(req.body.password);
-	res.send('Welcome ' + req.body.id + '('+req.body.password+')');
+	var email = req.body.email;
+	var password = req.body.password;
+	pool.query({
+		sql: 'SELECT * FROM Users WHERE email=? and password=?',
+		timeout: 4000,
+		values: [email, password]
+	}, function(error, results, fields){
+		if (error) {
+			res.send(error);
+		} else {
+			if (results.length > 0) {
+				//res.send(results);
+				res.send('Login succeed');
+			} 
+			else {
+				res.send(401, 'Login fail');
+			}
+			
+		}
+	});
+
 });
 
 router.post('/register', function(req, res, next) {
