0daf8e5d631ed91b8f586994e6c668d20f0aa659
yhkim <hohokim@Yonghoui-MacBookPro.local>
2019-11-12 14:19:26 +0100

    add chats management

--- app.js
+++ app.js
@@ -7,6 +7,7 @@ var logger = require('morgan');
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 var contentsRouter = require('./routes/contents');
+var chatsRouter = require('./routes/chats');
 
 var app = express();
 
@@ -24,6 +25,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 app.use('/contents', contentsRouter);
+app.use('/chats', chatsRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
